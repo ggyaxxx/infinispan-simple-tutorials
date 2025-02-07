594298ba8dd63234ea2607915dde4fc2645b08c8
Il tuo nome <tu@esempio.com>
2025-02-07 15:37:52 +0100

    .

--- infinispan-remote/opentelemetry/src/main/java/org/infinispan/tutorial/simple/remote/opentelemetry/InfinispanRemoteOpenTelemetry.java
+++ infinispan-remote/opentelemetry/src/main/java/org/infinispan/tutorial/simple/remote/opentelemetry/InfinispanRemoteOpenTelemetry.java
@@ -5,10 +5,12 @@ import org.infinispan.client.hotrod.RemoteCacheManager;
 import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
 import org.infinispan.tutorial.simple.connect.TutorialsConnectorHelper;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 import static org.infinispan.tutorial.simple.connect.TutorialsConnectorHelper.TUTORIAL_CACHE_NAME;
 
 /**
- * Shows how to enable tracing programatically in a cache, sends operations and traces
+ * Shows how to enable tracing programmatically in a cache, sends operations and traces
  * are displayed in Jaeger.
  *
  * Execute after running the docker-compose script in the docker-compose directory.
@@ -20,12 +22,23 @@ public class InfinispanRemoteOpenTelemetry {
       try (RemoteCacheManager client = TutorialsConnectorHelper.connect(builder)) {
          RemoteCache<String, String> cache = client.getCache(TUTORIAL_CACHE_NAME);
 
-         // Enabled tracing at runtime by changing the configuration.
+         // Enable tracing at runtime by changing the configuration.
          client.administration()
                  .updateConfigurationAttribute(cache.getName(), "tracing.enabled", "true");
 
          for (int i = 0; i < 300; i++) {
-            cache.put("i" + i, i + "");
+            String key = "i" + i;
+            String value = i + "";
+            System.out.println("inserting cache: "+ key + value);
+
+            cache.put(key, value);
+
+            // Introduce a variable delay between 10ms and 1200ms
+            try {
+               Thread.sleep(ThreadLocalRandom.current().nextInt(10, 1201));
+            } catch (InterruptedException e) {
+               Thread.currentThread().interrupt();
+            }
          }
 
          client.stop();
